5d669d6fbf30bdcec9b57c34d4c6a68744273999
netuz <netuzerr@gmail.com>
2020-02-26 14:39:33 +0200

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+
+<script>
+	let name = 'Anton';
+	let age = 33;
+
+	function incrementAge (){
+			age +=1;
+	};
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change age</button>
