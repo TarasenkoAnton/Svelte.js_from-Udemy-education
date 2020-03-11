0249cd181c551d705828bc97aca12662bc3585b1
netuz <netuzerr@gmail.com>
2020-03-11 14:28:50 +0200

    add event |preventDeafault modifier

--- src/App.svelte
+++ src/App.svelte
@@ -43,13 +43,14 @@
   #form {
     width: 30rem;
     max-width: 100%;
+    padding: 1rem;
   }
   .danger {
 	  color: red
   }
 </style>
 
-<div id="form">
+<form id="form">
   <div class="form-control">
     <label for="userName">User Name</label>
     <input type="text" bind:value={name} id="userName" />
@@ -66,8 +67,9 @@
     <label for="desc">Description</label>
     <textarea rows="3" bind:value={description} id="desc" />
   </div>
-</div>
-<button on:click="{addContact}">Add Contact Card</button>
+  <button on:click|preventDefault="{addContact}">Add Contact Card</button>
+</form>
+
 <button on:click="{deleteFirst}">Delete First</button>
 <button on:click="{deleteLast}">Delete Last</button>
 
